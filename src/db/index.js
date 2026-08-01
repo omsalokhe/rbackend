787a1b4a90c2d1075f46fe8b4af61ccd218cd367
omsalokhe <omsalokhe2020@gmail.com>
2026-08-01 23:16:45 +0530

    conncected database mongodb, error handling and routes also wriiten

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONOGODBURL}/${DB_NAME}`);
+    console.log(`\n mongodb connected!! DB host: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("Mongodb connection error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running at port :${process.env.PORT}`);
+    })
+})
+.catch((err)=<{
+    console.log("Mongo db connection failed !!!",err)
+})
+
+    // -r dotenv/config --experimental-json-module
